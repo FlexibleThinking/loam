9ef3e60d7a2fd74b1e1be3c7070c33abee962a0a
YousungJung <jys01012@gmail.com>
2021-06-08 20:04:58 +0900

    [Done] getter && setter

--- loam_velodyne-VLP-32C/src/lib/BasicLaserMappingKernel.cu
+++ loam_velodyne-VLP-32C/src/lib/BasicLaserMappingKernel.cu
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include <Eigen/Eigenvalues>
 #include <Eigen/QR>
-#include <time.h>
-#define MAX_BLOCK 255
+#define MAX_BLOCK 1024
+
 CudaTest::CudaTest(void)
 {
 }
@@ -14,14 +14,9 @@ CudaTest::~CudaTest(void)
 {
 }
 
-__global__ void sum_kernel(int a, int b, int *c)
-{
-	int tid = blockIdx.x * blockDim.x + threadIdx.x;
-	c[tid] = a + b;
-}
 __global__ void kernel_test(nanoflann::KdTreeFLANN<pcl::PointXYZI> kdtreeCornerFromMap, nanoflann::KdTreeFLANN<pcl::PointXYZI> kdtreeSurfFromMap)
-{
-
+{	// TODO
+	// write kernel code
 }
 void CudaTest::initialize()
 {
@@ -34,26 +29,33 @@ void CudaTest::initialize()
    	matV1.setZero();
 }
 void CudaTest::process()
-{
-	// write kernel code
+{   // TODO
+	// memcpy to device
+	// memcpy to host
 }
-/*int CudaTest::sum_cuda(int a, int b, int *c)
-{
-	int *f;
-	cudaMalloc((void**)&f, sizeof(int)* 1);
-	cudaMemcpy(f, c, sizeof(int)* 1, cudaMemcpyHostToDevice);
-
-	sum_kernel <<<1, 1>>>(a, b, f);
-
-	cudaMemcpy(c, f, sizeof(int) *1, cudaMemcpyDeviceToHost);
-
-	cudaFree(f);
-
-	return true;
-}*/
 void CudaTest::set_laser_cloud_corner_from_map(pcl::PointCloud<pcl::PointXYZI>::Ptr laserCloudCornerFromMapFromHost){
 	kdtreeCornerFromMap.setInputCloud(laserCloudCornerFromMapFromHost);
 }
 void CudaTest::set_laser_cloud_surf_from_map(pcl::PointCloud<pcl::PointXYZI>::Ptr laserCloudSurfFromMapFromHost){
 	kdtreeSurfFromMap.setInputCloud(laserCloudSurfFromMapFromHost);
 }
+
+void CudaTest::set_corner_stack_down_size(pcl::PointCloud<pcl::PointXYZI>::Ptr corner_from_host)
+{
+	_laserCloudCornerStackDS = corner_from_host;
+	laserCloudCornerStackNum = _laserCloudCornerStackDS->size();
+}
+void CudaTest::set_surf_stack_down_size(pcl::PointCloud<pcl::PointXYZI>::Ptr surf_from_host)
+{
+	_laserCloudSurfStackDS = surf_from_host;
+	laserCloudSurfStackNum = _laserCloudSurfStackDS->size();
+}
+
+void CudaTest::set_max_iter(size_t max_iter)
+{
+	_maxIterations = max_iter;
+}
+
+
+
+
