de35149931d3a7e84f1038ca35a6ec1495ee2401
YousungJung <jys01012@gmail.com>
2021-06-08 15:24:12 +0900

    remove files

---
+++ loam_velodyne-VLP-32C/src/lib/file1.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "loam_velodyne/file1.h"
+
+__global__ void mykernel(){
+	printf("Hello from mykernel\n");
+}
+
+int hello(){
+
+	mykernel<<<1,1>>>();
+	cudaDeviceSynchronize();
+	return 0;
+}
+
