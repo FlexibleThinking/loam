51e36267f3aa366d760df6df11f6f3e4d8f3bc02
YousungJung <jys01012@gmail.com>
2021-06-08 17:02:12 +0900

    [Trouble shooting]compiler error with eigen closed

--- loam_velodyne-VLP-32C/include/loam_velodyne/BasicLaserMappingKernel.cuh
+++ loam_velodyne-VLP-32C/include/loam_velodyne/BasicLaserMappingKernel.cuh
@@ -2,6 +2,18 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#if __CUDACC_VER_MAJOR__ >= 9
+#undef __CUDACC_VER__
+#define __CUDACC_VER__ \
+	  ((__CUDACC_VER_MAJOR__ * 10000) + (__CUDACC_VER_MINOR__ * 100))
+#endif
+
+#include "Twist.h"
+//#include "CircularBuffer.h"
+//#include <pcl/point_cloud.h>
+//#include <pcl/point_types.h>
+//#include <pcl/filters/voxel_grid.h>
+
 #ifdef __cplusplus
 extern "C"{
 #endif
