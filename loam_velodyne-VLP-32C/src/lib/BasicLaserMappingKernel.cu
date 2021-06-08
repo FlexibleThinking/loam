59ea7b09a2753fd4f70abdd63d5cbb6b98989a62
YousungJung <jys01012@gmail.com>
2021-06-08 18:46:06 +0900

    Initialize necessary variable

--- loam_velodyne-VLP-32C/src/lib/BasicLaserMappingKernel.cu
+++ loam_velodyne-VLP-32C/src/lib/BasicLaserMappingKernel.cu
@@ -1,5 +1,12 @@
 #include "loam_velodyne/BasicLaserMappingKernel.cuh"
+#include "math_utils.h"
+#include "loam_velodyne/nanoflann_pcl.h"
+
 #include <stdio.h>
+#include <Eigen/Eigenvalues>
+#include <Eigen/QR>
+#include <time.h>
+#define MAX_BLOCK 255
 CudaTest::CudaTest(void)
 {
 }
@@ -12,7 +19,25 @@ __global__ void sum_kernel(int a, int b, int *c)
 	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 	c[tid] = a + b;
 }
-int CudaTest::sum_cuda(int a, int b, int *c)
+__global__ void kernel_test(nanoflann::KdTreeFLANN<pcl::PointXYZI> kdtreeCornerFromMap, nanoflann::KdTreeFLANN<pcl::PointXYZI> kdtreeSurfFromMap)
+{
+
+}
+void CudaTest::initialize()
+{
+	matA0.setZero();
+   	matB0.setConstant(-1);
+   	matX0.setZero();
+
+   	matA1.setZero();
+   	matD1.setZero();
+   	matV1.setZero();
+}
+void CudaTest::process()
+{
+	// write kernel code
+}
+/*int CudaTest::sum_cuda(int a, int b, int *c)
 {
 	int *f;
 	cudaMalloc((void**)&f, sizeof(int)* 1);
@@ -25,4 +50,10 @@ int CudaTest::sum_cuda(int a, int b, int *c)
 	cudaFree(f);
 
 	return true;
+}*/
+void CudaTest::set_laser_cloud_corner_from_map(pcl::PointCloud<pcl::PointXYZI>::Ptr laserCloudCornerFromMapFromHost){
+	kdtreeCornerFromMap.setInputCloud(laserCloudCornerFromMapFromHost);
+}
+void CudaTest::set_laser_cloud_surf_from_map(pcl::PointCloud<pcl::PointXYZI>::Ptr laserCloudSurfFromMapFromHost){
+	kdtreeSurfFromMap.setInputCloud(laserCloudSurfFromMapFromHost);
 }
